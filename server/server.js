56a0bc4b8a71519942330c769880406f739d3567
impersin <closer124@gmail.com>
2017-09-22 17:44:19 -0700

    Initialized server with wepack middlewares

---
+++ server/server.js
@@ -0,0 +1,28 @@
+ var express = require('express');
+ var path = require('path');
+ var config = require('../webpack.config.js');
+ var webpack = require('webpack');
+ var webpackDevMiddleware = require('webpack-dev-middleware');
+ var webpackHotMiddleware = require('webpack-hot-middleware');
+ var publicPath = path.resolve(__dirname, '/../client');
+
+ var app = express();
+
+ var compiler = webpack(config);
+
+ app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
+ app.use(webpackHotMiddleware(compiler));
+
+
+ app.use(express.static(publicPath));
+
+ app.use('/', function (req, res) {
+   res.sendFile(path.resolve('client/index.html'));
+ });
+
+ var port = 3000;
+
+ app.listen(port, function(error) {
+   if (error) { throw error; }
+   console.log('Express server listening on port', port);
+ });  
